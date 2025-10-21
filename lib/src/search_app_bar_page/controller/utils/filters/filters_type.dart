b9af8e903a6e5c1222f6cf95daf86754069b2fd1
MarceloRab <amominhache@yahoo.com.br>
2025-10-21 18:58:21 -0300

    chore: commit only tracked file changes

--- lib/src/search_app_bar_page/controller/utils/filters/filters_type.dart
+++
@@ -1,5 +0,0 @@
-enum FiltersTypes { startsWith, equals, contains }
-
-typedef StringFilter<T> = String? Function(T test);
-typedef FunctionRefresh<T> = Future<List<T>> Function();
-typedef WhereFilter<T> = bool Function(T test, String? query);
--- test/infra_fakes_data/future_pagination.dart
+++
@@ -1,6 +0,0 @@
-typedef FutureFetchPageItems<T> = Future<List<T>> Function(
-    int page, String query);
-
-void runFuturePagination<T>(FutureFetchPageItems<T> futureFunction) {
-  //futureFunction().then((value) => null);
-}
